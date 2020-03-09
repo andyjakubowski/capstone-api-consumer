436e39a71049fad924379d2bb80e4e24370b95cb
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-09 10:35:58 +0100

    Update CSS with JS based on Token API

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,54 +16,54 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-const root = document.documentElement;
+// const root = document.documentElement;
 
-const applyTheme = function applyTheme(listId) {
-  fetch(`https://andy-capstone-week-03.herokuapp.com/lists/${listId}/tokens.json`)
-    .then((response) => response.json())
-    .then((jsObject) => updateCSSProperties(jsObject));
-};
+// const applyTheme = function applyTheme(listId) {
+//   fetch(`http://localhost:3000/lists/${1}/tokens.json`)
+//     .then((response) => response.json())
+//     .then((jsObject) => updateCSSProperties(jsObject));
+// };
 
-const applyLightTheme = function applyLightTheme() {
-  localStorage.setItem('activeTheme', 'light');
-  applyTheme(6);
-};
+// const applyLightTheme = function applyLightTheme() {
+//   localStorage.setItem('activeTheme', 'light');
+//   applyTheme(6);
+// };
 
-const applyDarkTheme = function applyDarkTheme() {
-  localStorage.setItem('activeTheme', 'dark');
-  applyTheme(7);
-};
+// const applyDarkTheme = function applyDarkTheme() {
+//   localStorage.setItem('activeTheme', 'dark');
+//   applyTheme(7);
+// };
 
-const updateCSSProperties = function updateCSSProperties(tokens) {
-  tokens.forEach(({ name, value }) => {
-    root.style.setProperty(`--remote-${name}`, value);
-  });
-}
+// const updateCSSProperties = function updateCSSProperties(tokens) {
+//   tokens.forEach(({ name, value }) => {
+//     root.style.setProperty(`--remote-${name}`, value);
+//   });
+// }
 
-const app = (function buildApp() {
-  return {
-    init() {
-      console.log(`document.readyState: ${document.readyState}`);
-      console.log('app init');
+// const app = (function buildApp() {
+//   return {
+//     init() {
+//       console.log(`document.readyState: ${document.readyState}`);
+//       console.log('app init');
 
-      const activeTheme = localStorage.getItem('activeTheme');
-      const lightThemeButton = document.getElementById('light-theme-button');
-      const darkThemeButton = document.getElementById('dark-theme-button');
+//       const activeTheme = localStorage.getItem('activeTheme');
+//       const lightThemeButton = document.getElementById('light-theme-button');
+//       const darkThemeButton = document.getElementById('dark-theme-button');
 
-      lightThemeButton.addEventListener('click', applyLightTheme);
-      darkThemeButton.addEventListener('click', applyDarkTheme);
+//       lightThemeButton.addEventListener('click', applyLightTheme);
+//       darkThemeButton.addEventListener('click', applyDarkTheme);
 
-      if (activeTheme === 'light') {
-        applyLightTheme();
-      } else {
-        applyDarkTheme();
-      }
-    }
-  };
-}());
+//       if (activeTheme === 'light') {
+//         applyLightTheme();
+//       } else {
+//         applyDarkTheme();
+//       }
+//     }
+//   };
+// }());
 
-if (document.readyState !== 'loading') {
-  app.init();
-} else {
-  document.addEventListener('DOMContentLoaded', app.init)
-}
+// if (document.readyState !== 'loading') {
+//   app.init();
+// } else {
+//   document.addEventListener('DOMContentLoaded', app.init)
+// }
