d41afd42f21328f7846f3526f14c1451b461a2ba
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-10 13:38:02 +0100

    Work on TokenHub API connection

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 const App = (function App() {
+  const SPACE_NAME = 'cod-wolf-crab';
   const root = document.documentElement;
   let ul;
 
   const getLists = function getLists() {
-    return fetch('http://localhost:3027/spaces/snail-giraffe-alligator.json')
+    return fetch(`http://localhost:3027/spaces/${SPACE_NAME}.json`)
       .then((response) => response.json());
   };
 
@@ -63,59 +64,3 @@ if (document.readyState !== 'loading') {
 } else {
   document.addEventListener('DOMContentLoaded', App.init)
 }
-
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-// const root = document.documentElement;
-
-// const applyTheme = function applyTheme(listId) {
-//   fetch(`http://localhost:3000/lists/${1}/tokens.json`)
-//     .then((response) => response.json())
-//     .then((jsObject) => updateCSSProperties(jsObject));
-// };
-
-// const applyLightTheme = function applyLightTheme() {
-//   localStorage.setItem('activeTheme', 'light');
-//   applyTheme(6);
-// };
-
-// const applyDarkTheme = function applyDarkTheme() {
-//   localStorage.setItem('activeTheme', 'dark');
-//   applyTheme(7);
-// };
-
-// const updateCSSProperties = function updateCSSProperties(tokens) {
-//   tokens.forEach(({ name, value }) => {
-//     root.style.setProperty(`--remote-${name}`, value);
-//   });
-// }
-
-// const app = (function buildApp() {
-//   return {
-//     init() {
-//       console.log(`document.readyState: ${document.readyState}`);
-//       console.log('app init');
-
-//       const activeTheme = localStorage.getItem('activeTheme');
-//       const lightThemeButton = document.getElementById('light-theme-button');
-//       const darkThemeButton = document.getElementById('dark-theme-button');
-
-//       lightThemeButton.addEventListener('click', applyLightTheme);
-//       darkThemeButton.addEventListener('click', applyDarkTheme);
-
-//       if (activeTheme === 'light') {
-//         applyLightTheme();
-//       } else {
-//         applyDarkTheme();
-//       }
-//     }
-//   };
-// }());
-
